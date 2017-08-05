a631b20cc9ddc5593d4a19ca2f4471c3e1310d79
Ganesh <ganesh.alkurn@gmail.com>
2017-08-05 15:02:55 +0530

    update

--- WebSocket.php
+++ WebSocket.php
@@ -9,9 +9,8 @@ class WebSocket extends Component
     public $host;
     public $port;
 
-    public $socket;
-    public $socket_new;
     public $clients;
+    public $verboseMode = false;
 
     public function init()
     {
@@ -19,95 +18,143 @@ class WebSocket extends Component
         $port = $this->port;
     }
 
-    public function createSocket(){
+    public function createSocket()
+    {
 
         $host = \Yii::$app->WebSocket->host;
         $port = \Yii::$app->WebSocket->port;
 
         $null = NULL; //null var
         //Create TCP/IP sream socket
-        $this->socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 
         //reuseable port
-        socket_set_option($this->socket, SOL_SOCKET, SO_REUSEADDR, 1);
+        socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
+
+        if (!is_resource($socket)) {
+            $this->console("socket_create() failed: " . socket_strerror(socket_last_error()), true);
+        }
 
         //bind socket to specified host
-        socket_bind($this->socket, 0, $port);
+        if (!socket_bind($socket, $host, $port)) {
+            $this->console("socket_bind() failed: " . socket_strerror(socket_last_error()), true);
+        }
 
         //listen to port
-        socket_listen($this->socket);
+        if (!socket_listen($socket, 20)) {
+            $this->console("socket_listen() failed: " . socket_strerror(socket_last_error()), true);
+        }
 
         //create & add listning socket to the list
-        $this->clients = array($this->socket);
+        $this->clients = [$socket];
 
         //start endless loop, so that our script doesn't stop
         while (true) {
 
             //manage multipal connections
-
             $changed = $this->clients;
-            //returns the socket resources in $changed array
-            socket_select($changed, $null, $null, 0, 10);
 
-            //check for new socket
-            if (in_array($this->socket, $changed)) {
-                $this->socket_new = socket_accept($this->socket); //accpet new socket
-                $this->clients[] = $this->socket_new; //add socket to client array
+            if (!empty($changed) && is_array($changed) && count($changed) > 0) {
 
-                $header = socket_read($this->socket_new, 1024); //read data sent by the socket
-                $this->performHandshaking($header, $this->socket_new, $host, $port); //perform websocket handshake
+                //returns the socket resources in $changed array
+                @socket_select($changed, $null, $null, 0, 10);
 
-                socket_getpeername($this->socket_new, $ip); //get ip address of connected socket
+               /* if (false === socket_select($changed, $null, $null, 0, 10)) {
+                    echo "socket_select() failed, reason: " .
+                        socket_strerror(socket_last_error()) . "\n";
+                    exit;
+                }*/
 
-                $response = $this->mask(json_encode(['type' => 'system', 'message' => $ip . ' connected']));//prepare json data
-                $this->sendMessage($response); //notify all users about new connection
-
-                //make room for new socket
-                $found_socket = array_search($this->socket, $changed);
-                unset($changed[$found_socket]);
-            }
-
-            //loop through all connected sockets
-            foreach ($changed as $changed_socket) {
-
-                //check for any incomming data
-                while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
-
-                    $received_text = $this->unmask($buf); //unmask data
-                    $tst_msg = json_decode($received_text); //json decode
-
-                    if ($tst_msg && count($tst_msg) > 0) {
-                        $type = (isset($tst_msg->type) && !empty($tst_msg->type)) ? $tst_msg->type : 'user';
-                        $info = ['code' => 'success', 'type' => $type, 'message_id' => $tst_msg->message_id, 'seq' => $tst_msg->seq, 'text' => $tst_msg->text];
-                        //prepare data to be sent to client
-                    } else {
-                        $info = ['code' => 'failed', 'message_id' => null, 'seq' => null, 'text' => 'Error in socket'];
+                //check for new socket
+                if (in_array($socket, $changed)) {
+                    if (($socket_new = socket_accept($socket)) < 0) {
+                        $this->console("Socket error: " . socket_strerror(socket_last_error($socket_new)));
+                        print_R("Socket error: " . socket_strerror(socket_last_error($socket_new)));
+                        exit;
                     }
 
-                    $response_text = $this->mask(json_encode($info));
-                    $this->sendMessage($response_text); //send data
-                    break 2; //exist this loop
+                    $this->clients[] = $socket_new; //add socket to client array
+                    $header = socket_read($socket_new, 1024); //read data sent by the socket
+
+                    $this->performHandshaking($header, $socket_new, $host, $port); //perform websocket handshake
+
+                    //get ip address of connected socket
+                    socket_getpeername($socket_new, $ip);
+
+                    //prepare json data
+                    $response = $this->mask(json_encode(['type' => 'system', 'message' => $ip . ' connected']));
+
+
+                    $this->sendMessage($response); //notify all users about new connection
+
+                    //make room for new socket
+                    $found_socket = array_search($socket, $changed);
+                    unset($changed[$found_socket]);
                 }
 
-                $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
-                if ($buf === false) {
+                //loop through all connected sockets
+                foreach ($changed as $changed_socket) {
 
-                    // check disconnected client
-                    // remove client for $clients array
+                    //check for any incomming data
+                    while (socket_recv($changed_socket, $buf, 1024, 0) >= 1) {
 
-                    $found_socket = array_search($changed_socket, $this->clients);
-                    socket_getpeername($changed_socket, $ip);
-                    unset($this->clients[$found_socket]);
+                        $received_text = $this->unmask($buf); //unmask data
+                        $tst_msg = json_decode($received_text); //json decode
 
-                    //notify all users about disconnected connection
-                    $response = $this->mask(json_encode(['code' => 'failed', 'type' => 'system', 'message' => $ip . ' disconnected']));
-                    $this->sendMessage($response);
+                        if ($tst_msg && count($tst_msg) > 0) {
+                            $type = (isset($tst_msg->type) && !empty($tst_msg->type)) ? $tst_msg->type : 'user';
+                            $info = ['code' => 'success', 'type' => $type, 'message_id' => $tst_msg->message_id, 'seq' => $tst_msg->seq, 'text' => $tst_msg->text];
+
+                            //prepare data to be sent to client
+                        } else {
+                            $info = ['code' => 'failed', 'message_id' => null, 'seq' => null, 'text' => 'Error in socket'];
+                        }
+
+                        $response_text = $this->mask(json_encode($info));
+                        $this->sendMessage($response_text); //send data
+                        break 2; //exist this loop
+                    }
+
+                    $buf = @socket_read($changed_socket, 1024, PHP_NORMAL_READ);
+                    if ($buf === false) {
+
+                        // check disconnected client
+                        // remove client for $clients array
+
+                        $found_socket = array_search($changed_socket, $this->clients);
+                        socket_getpeername($changed_socket, $ip);
+                        unset($this->clients[$found_socket]);
+
+                        //notify all users about disconnected connection
+                        $response = $this->mask(json_encode(['code' => 'failed', 'type' => 'system', 'message' => $ip . ' disconnected']));
+                        $this->sendMessage($response);
+                    }
                 }
             }
         }
+
         // close the listening socket
-        socket_close($this->socket);
+        socket_close($socket);
     }
+
+    /**
+     * Print a text to the terminal
+     * @param $text the text to display
+     * @param $exit if true, the process will exit
+     */
+    private function console($text, $exit = false)
+    {
+        $text = date('[Y-m-d H:i:s] ') . $text . "\r\n";
+        if ($exit) {
+            die($text);
+        }
+
+        if ($this->verboseMode) {
+            echo $text;
+        }
+    }
+
+//Unmask incoming framed message
 
     function performHandshaking($receved_header, $client_conn, $host, $port)
     {
@@ -121,7 +168,7 @@ class WebSocket extends Component
             }
         }
 
-        $secKey = $headers['Sec-WebSocket-Key'];
+        $secKey = isset($headers['Sec-WebSocket-Key']) ? $headers['Sec-WebSocket-Key'] : null;
         $secAccept = base64_encode(pack('H*', sha1($secKey . '258EAFA5-E914-47DA-95CA-C5AB0DC85B11')));
         //hand shaking header
         $upgrade = "HTTP/1.1 101 Web Socket Protocol Handshake\r\n" .
@@ -134,7 +181,7 @@ class WebSocket extends Component
         socket_write($client_conn, $upgrade, strlen($upgrade)) or die("Could not write outputs\n");
     }
 
-//Unmask incoming framed message
+//Encode message for transfer to client.
 
     function mask($text)
     {
@@ -147,7 +194,8 @@ class WebSocket extends Component
         return $header . $text;
     }
 
-//Encode message for transfer to client.
+
+//handshake new client.
 
     function sendMessage($msg)
     {
@@ -159,9 +207,6 @@ class WebSocket extends Component
 
         return true;
     }
-
-
-//handshake new client.
 
     function unmask($text)
     {
